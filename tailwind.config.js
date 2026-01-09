eb97f618d1862175e2263bcd9cb53839ba4f0f2c
nexusgamers-lab <ettouilissam2@gmail.com>
2026-01-09 11:19:02 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        nexusCyan: '#00f3ff',
+        nexusPurple: '#bc13fe',
+      },
+    },
+  },
+  plugins: [],
+}
